cbbb49845627bf10fd71e2497eec7a4c3a46115b
untodesu <41549137+untodesu@users.noreply.github.com>
2024-12-13 19:38:28 +0500

    Disable stderr sink when syslog is involved

--- source/game/shared/setup.cc
+++ source/game/shared/setup.cc
@@ -87,7 +87,6 @@ void shared::setup(int argc, char **argv)
 #if defined(__unix__)
     if(cmdline::contains("use-syslog")) {
         logger_sinks.clear();
-        logger_sinks.push_back(std::make_shared<spdlog::sinks::stderr_color_sink_mt>());
         logger_sinks.push_back(std::make_shared<spdlog::sinks::syslog_sink_mt>("voxelius", (LOG_PID | LOG_NDELAY), LOG_USER, true));
     }
     else {
